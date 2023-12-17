df55ba00a3eea9e00e3e07a9abab5f52fad34073
Giovanni Billa <giovanni.billa01@gmail.com>
2023-12-16 21:53:19 -0300

    message
    created class message and authenticator process

--- auth.php
+++ auth.php
@@ -22,7 +22,7 @@
             </div>
             <div class="col-md-4" id="register-container">
                 <h2>Criar Conta</h2>
-                <form action="" method="post">
+                <form action="<?= $BASE_URL ?>auth_process.php" method="post">
                     <input type="hidden" name="type" value="register">
                     <div class="form-group">
                         <label for="email">E-mail:</label>
